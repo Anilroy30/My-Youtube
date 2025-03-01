22a3895f5cf3ad324f8578f0748bf3d2e2fb3a6b
Striver <anilkumarakula34@gmail.com>
2025-03-01 22:42:47 +0530

    built live chat by API Polling and redux

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,24 +1,50 @@
+import { useDispatch, useSelector } from 'react-redux';
 import ChatMessage from './ChatMessage'
-import react, { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { addMessage } from '../utils/chatSlice';
+import { generateRandomName, makeRandomMessage } from '../utils/helper';
 
 const LiveChat = () => {
+    const [liveMessage, setLiveMessage] = useState("");
+    const disPatch = useDispatch();
+
+    const chatMessages = useSelector((store) => store.chat.messages);
+
     useEffect(() => {
         const i = setInterval(() => {
             // API Polling
-            console.log('Polling for new messages');
+            disPatch(addMessage({
+                name: generateRandomName(),
+                message: makeRandomMessage(30)
+            }))
         }, 2000);
 
         return () => clearInterval(i);
     }, []);
 
   return (
-    <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg'>
-        <ChatMessage name="Arjun" message="Hello World"/>
-        <ChatMessage name="Arjun" message="Hello World"/>
-        <ChatMessage name="Arjun" message="Hello World"/>
-        <ChatMessage name="Arjun" message="Hello World"/>
-        <ChatMessage name="Arjun" message="Hello World"/>
+    <>
+    <div className=' w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse'>
+        {chatMessages.map((c, index) => <ChatMessage
+            key={index}
+            name={c.name} 
+            message={c.message}/>
+        )}
     </div>
+
+    <form class="w-full p-2 ml-2 border border-black" onSubmit={(e) => {
+        e.preventDefault();
+        disPatch(addMessage({
+            name: "Striver",
+            message: liveMessage
+        }));
+        setLiveMessage("");
+    }} >
+        <input className='w-96' type="text" value={liveMessage}
+        onChange={(e) => setLiveMessage(e.target.value)}/>
+        <button className='px-2 mx-2 bg-green-100'>Send</button>
+    </form>
+    </>
   )
 }
 
